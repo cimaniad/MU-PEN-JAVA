5f7fd41867e58f5ffe7c6bbeff087db7f0298d2c
Ricardo Araújo <ricardo.mga24@gmail.com>
2015-04-08 15:46:53 +0100

    correção dos nomes das variaveis

--- src/frontend/terapeuta/PerfilPaciente.java
+++ src/frontend/terapeuta/PerfilPaciente.java
@@ -3,19 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package frontend.terapeuta;
 
 /**
  *
  * @author Quina
  */
-public class Perfil_Paciente extends javax.swing.JFrame {
+public class PerfilPaciente extends javax.swing.JFrame {
 
     /**
      * Creates new form Perfil_Paciente
      */
-    public Perfil_Paciente() {
+    public PerfilPaciente() {
         initComponents();
     }
 
@@ -210,20 +209,20 @@ public class Perfil_Paciente extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Perfil_Paciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PerfilPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Perfil_Paciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PerfilPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Perfil_Paciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PerfilPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Perfil_Paciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PerfilPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Perfil_Paciente().setVisible(true);
+                new PerfilPaciente().setVisible(true);
             }
         });
     }
--- src/frontend/terapeuta/RegistarPaciente.java
+++ src/frontend/terapeuta/RegistarPaciente.java
@@ -10,12 +10,12 @@ package frontend.terapeuta;
  *
  * @author Quina
  */
-public class Registar_Paciente extends javax.swing.JFrame {
+public class RegistarPaciente extends javax.swing.JFrame {
 
     /**
      * Creates new form Registar_Paciente
      */
-    public Registar_Paciente() {
+    public RegistarPaciente() {
         initComponents();
     }
 
@@ -279,20 +279,20 @@ public class Registar_Paciente extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Registar_Paciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistarPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Registar_Paciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistarPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Registar_Paciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistarPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Registar_Paciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RegistarPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Registar_Paciente().setVisible(true);
+                new RegistarPaciente().setVisible(true);
             }
         });
     }
