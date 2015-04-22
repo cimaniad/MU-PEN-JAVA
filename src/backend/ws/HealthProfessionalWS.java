e560fd0e06e83a2cc2b05730f22a4a00dc6054b0
Ricardo Araújo <ricardo.mga24@gmail.com>
2015-04-22 01:25:45 +0100

    commit inicial projecto

--- src/backend/ws/HealthProfessionalWS.java
+++ src/backend/ws/HealthProfessionalWS.java
@@ -31,7 +31,7 @@ public class HealthProfessionalWS {
 
     public HealthProfessionalWS() {
         gson = new Gson();
-        wrapperWS = WrapperWS.getConexao();
+        wrapperWS = WrapperWS.getWrapperWS();
         
     }
 
